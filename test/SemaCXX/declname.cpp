bc584c90e0bd9165a955e8b2915333ef9fd81937
Sebastian Redl <sebastian.redl@getdesigned.at>
2014-06-24 17:51:30 +0200

    Remove error annotations for declname literals in test.

--- test/SemaCXX/declname.cpp
+++ test/SemaCXX/declname.cpp
@@ -1,12 +1,12 @@
 // RUN: %clang_cc1 -std=c++11 -fsyntax-only -verify %s
 
-int main() {
+void test() {
   (void)``; // expected-error {{expected unqualified-id}}
   (void)`foo bar`; // expected-error {{expected '`'}}
   (void)`+`; // expected-error {{expected unqualified-id}}
-  (void)`foo`; // expected-error {{declname literals are not yet supported}}
-  (void)` foo`; // expected-error {{declname literals are not yet supported}}
-  (void)`foo `; // expected-error {{declname literals are not yet supported}}
-  (void)`operator+`; // expected-error {{declname literals are not yet supported}}
-  (void)`template foo<int>`; // expected-error {{declname literals are not yet supported}}
+  (void)`foo`;
+  (void)` foo`;
+  (void)`foo `;
+  (void)`operator+`;
+  (void)`template foo<int>`;
 }
